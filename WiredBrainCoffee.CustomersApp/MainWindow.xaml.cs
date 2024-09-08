4529c01304769e17ae42b8a537ec03d6e0b9a3ac
brian <brian.baecher@gmail.com>
2024-09-08 17:18:06 -0400

    Add project files.

---
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WiredBrainCoffee.CustomersApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+		{
+			btnAddCustomer.Content = "Customer edit";
+		}
+
+		private void ButtonMoveCustomerList_Click(object sender, RoutedEventArgs e)
+		{
+			//var column = (int)customerListGridElement.GetValue(Grid.ColumnProperty);
+
+			//var newColumn = column == 0 ? 2 : 0;
+
+			//customerListGridElement.SetValue(Grid.ColumnProperty, newColumn);
+
+			var column = Grid.GetColumn(customerListGridElement);
+
+			var newColumn = column == 0 ? 2 : 0;
+
+			Grid.SetColumn(customerListGridElement, newColumn);
+		}
+	}
+}
